d878c3518d56f84bfdefe10439c750450340b536
Fonsii <lualfonso14200174@gmail.com>
2021-11-23 18:11:33 -0600

    init

---
+++ Cliente/HolaMundo.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+namespace HolaMundo
+{
+	/// <summary>
+	/// Descripción breve de Form1.
+	/// </summary>
+	public class frmHolaMundo : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.Button cmdSaludar;
+		private System.Windows.Forms.Button cmdUltimoSaludo;
+		private System.Windows.Forms.Label lblNombre;
+		/// <summary>
+		/// Variable del diseñador requerida.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		// private HolaMundo.ECCI_HolaMundo.ECCI_HolaMundo ws; // .NET 2.0
+		private HolaMundo.ECCI_HolaMundo.ECCI_HolaMundoPortClient ws;
+
+		public frmHolaMundo()
+		{
+			//
+			// Necesario para admitir el Diseñador de Windows Forms
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Agregar código de constructor después de llamar a InitializeComponent
+			//
+			ws = new HolaMundo.ECCI_HolaMundo.ECCI_HolaMundoPortClient();
+			//Esto es con .NET 2.0
+			//ws.CookieContainer = new System.Net.CookieContainer();
+		}
+
+		/// <summary>
+		/// Limpiar los recursos que se estén utilizando.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if (components != null) 
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador, no se puede modificar
+		/// el contenido del método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.cmdSaludar = new System.Windows.Forms.Button();
+			this.lblNombre = new System.Windows.Forms.Label();
+			this.cmdUltimoSaludo = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtNombre
+			// 
+			this.txtNombre.Location = new System.Drawing.Point(32, 64);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(232, 20);
+			this.txtNombre.TabIndex = 0;
+			this.txtNombre.Text = "";
+			// 
+			// cmdSaludar
+			// 
+			this.cmdSaludar.Location = new System.Drawing.Point(32, 104);
+			this.cmdSaludar.Name = "cmdSaludar";
+			this.cmdSaludar.TabIndex = 1;
+			this.cmdSaludar.Text = "Saludar";
+			this.cmdSaludar.Click += new System.EventHandler(this.cmdSaludar_Click);
+			// 
+			// lblNombre
+			// 
+			this.lblNombre.Location = new System.Drawing.Point(32, 48);
+			this.lblNombre.Name = "lblNombre";
+			this.lblNombre.Size = new System.Drawing.Size(100, 16);
+			this.lblNombre.TabIndex = 2;
+			this.lblNombre.Text = "Nombre:";
+			// 
+			// cmdUltimoSaludo
+			// 
+			this.cmdUltimoSaludo.Location = new System.Drawing.Point(152, 104);
+			this.cmdUltimoSaludo.Name = "cmdUltimoSaludo";
+			this.cmdUltimoSaludo.Size = new System.Drawing.Size(88, 23);
+			this.cmdUltimoSaludo.TabIndex = 3;
+			this.cmdUltimoSaludo.Text = "Último Saludo";
+			this.cmdUltimoSaludo.Click += new System.EventHandler(this.cmdUltimoSaludo_Click);
+			// 
+			// frmHolaMundo
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(292, 150);
+			this.Controls.AddRange(new System.Windows.Forms.Control[] {
+																		  this.cmdUltimoSaludo,
+																		  this.lblNombre,
+																		  this.cmdSaludar,
+																		  this.txtNombre});
+			this.Name = "frmHolaMundo";
+			this.Text = "Hola Mundo";
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		/// <summary>
+		/// Punto de entrada principal de la aplicación.
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new frmHolaMundo());
+		}
+
+		private void cmdSaludar_Click(object sender, System.EventArgs e)
+		{
+			String Mensaje = ws.salude(txtNombre.Text);
+			MessageBox.Show(Mensaje);
+		}
+
+		private void cmdUltimoSaludo_Click(object sender, System.EventArgs e)
+		{
+			String Mensaje = ws.ultimoSaludo();
+			MessageBox.Show(Mensaje);
+		}
+
+	}
+}
---
+++ Servidor/TicTacToe-RPC-Encoded/TicTacToe.class.php
@@ -0,0 +1,109 @@
+<?php
+  
+/**
+ * 
+ * Copyright (c) 2005-2015, Braulio Jos� Solano Rojas
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ * 	Redistributions of source code must retain the above copyright notice, this list of
+ * 	conditions and the following disclaimer. 
+ * 	Redistributions in binary form must reproduce the above copyright notice, this list of
+ * 	conditions and the following disclaimer in the documentation and/or other materials
+ * 	provided with the distribution. 
+ * 	Neither the name of the Solsoft de Costa Rica S.A. nor the names of its contributors may
+ * 	be used to endorse or promote products derived from this software without specific
+ * 	prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *
+ * @version $Id$
+ * @copyright 2005-2015
+ */
+
+
+/**
+ * HolaMundo Clase que implementa el t�pico primer ejemplo de programaci�n en todo lenguaje.
+ * 
+ * @package SoapDiscovery
+ * @author Braulio Jos� Solano Rojas
+ * @copyright Copyright (c) 2005-2015 Braulio Jos� Solano Rojas
+ * @version $Id$
+ * @access public
+ **/
+class TicTacToe {
+    private $board = array(-1, -1, -1, -1, -1, -1, -1, -1, -1);
+	private $nextMove = 0;
+
+	/**
+	 * TicTacToe::__construct() Constructor de la clase TicTacToe.
+	 * 
+	 * @param int $nextMove
+	 **/
+	public function __construct($nextMove = 0) {
+		$this->nextMove = $nextMove;
+	}
+
+    /**
+     * TicTacToe::doMove() Realiza el siguiente movimiento.
+     * 
+     * @param int $move
+     * @return string
+     **/
+    public function doMove($move){
+        if ($this->nextMove == 0){
+            $this->nextMove = 1;
+            return $this->playerMove($move);
+        }else{
+            $this->nextMove = 0;
+            return $this->computerMove();
+        }
+    }
+	
+	/**
+	 * TicTacToe::playerMove() Realiza el movimiento del jugador.
+	 * 
+	 * @param int $move
+	 * @return string
+	 **/
+    private function playerMove($move) {
+        if ($this->board[$move] == -1){
+            $this->board[$move] = 0;
+            return "0";
+        }
+        else{
+            return "1";
+        }
+    }
+
+    /**
+	 * TicTacToe::computerMove() Realiza el movimiento de la computadora.
+	 * 
+	 * @return string
+	 **/
+    private function computerMove() {
+        $move = rand(0,8);
+        while ($this->board[$move] != -1) {
+            $move = rand(0,8);
+        }
+        $this->board[$move] = 1;
+        return "0";
+    }
+}
+
+?>
